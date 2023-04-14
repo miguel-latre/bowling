dba3266b4f8cd8d1e8543cf2a87e0a36789b81fc
Miguel Ángel Latre Abadía <latre@unizar.es>
2023-04-14 11:52:01 +0200

    deleted unused imports

--- test/es/unizar/eina/vv/bowling/GameTest.java
+++ test/es/unizar/eina/vv/bowling/GameTest.java
@@ -2,10 +2,6 @@ package es.unizar.eina.vv.bowling;
 
 import org.junit.jupiter.api.*;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
