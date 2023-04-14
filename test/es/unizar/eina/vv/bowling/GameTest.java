1996c377cd2b48caa077a18f4e48cae0e463bea9
Miguel Ángel Latre Abadía <latre@unizar.es>
2023-04-14 11:50:32 +0200

    Initial commit

---
+++ test/es/unizar/eina/vv/bowling/GameTest.java
@@ -0,0 +1,16 @@
+package es.unizar.eina.vv.bowling;
+
+import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+    @Test
+    public void test() {
+        fail();
+    }
+}
